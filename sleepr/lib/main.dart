9d409760acfab2dc2e3d7dcf345d05fe1c1f242d
Varghese Rony George <varghese.rony.george@gmail.com>
2022-01-15 14:19:54 -0500

    Remove appbar from main.dart

--- sleepr/lib/main.dart
+++ sleepr/lib/main.dart
@@ -77,13 +77,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
