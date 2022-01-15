1fb92d8fbb12f630fdb92abbfa0579c3e6e738cf
Varghese Rony George <varghese.rony.george@gmail.com>
2022-01-15 14:18:07 -0500

    T0: Separate Functionality of Calendar from main.dart

--- sleepr/lib/calendar.dart
+++ sleepr/lib/calendar.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-// class Calendar extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(child: Text("Welcome"),
-//       ),
-//     );
-//   }
-// }
+class Calendar extends StatefulWidget  {
+  const Calendar({Key? key}) : super(key: key);
+
+ @override
+  _Calendar createState() => _Calendar();
+
+
+}
+
+class _Calendar extends State<Calendar> {
+  CalendarFormat format = CalendarFormat.month;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TableCalendar(
+            focusedDay: DateTime.now(), 
+            firstDay: DateTime(1990), 
+            lastDay: DateTime(2050),
+            onFormatChanged: (CalendarFormat _format) {
+              setState(() {
+                format = _format;
+              });
+            },
+          )
+       ],
+    );
+  }
+}
